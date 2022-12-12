624188a70d84649254558785774291b683d8d2db
chamikarasamarasekara <chamikarasamrasekara@gmail.com>
2022-12-12 14:15:26 +0530

    uploaded auth config

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 function sendMessage(req,res){
     var data = JSON.stringify({
         "message": req.query.SmsMessage,
-        "phoneNumber": req.query.PhoneNumber
+        "phoneNumber": req.query.PhoneNumber.replace("+")
     });
 
     var config = {
